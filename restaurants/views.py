f07812a3a8ea76e405ce5255884f3bd185b0f51f
Maram Kamil <maram.kamil@gmail.com>
2019-02-10 16:35:07 +0300

    Task 3

--- restaurants/views.py
+++ restaurants/views.py
@@ -6,6 +6,20 @@ def welcome(request):
 def restaurant_list(request):
 
     context = {
+    	'my_list' : [
+    		{
+    		"restaurant_name" : "Nino",
+    		"food_type" : "Italian",
+    		},
+    		{
+    		"restaurant_name" : "Shrimp Anatomy",
+    		"food_type" : "Sea Food",
+    		},
+    		{
+    		"restaurant_name" : "Pizzahut",
+    		"food_type" : "Pizza!",
+    		},
+    	]
 
     }
     return render(request, 'list.html', context)
@@ -14,6 +28,17 @@ def restaurant_list(request):
 def restaurant_detail(request):
 
     context = {
-
+    	'my_object' : {
+    		"restaurant_name" : "Sports Cafe",
+    		"food_type" : "Deserts, All Kind of Food",
+    	},
+    	'my_object2' : {
+    		"restaurant_name" : "Juice Time",
+    		"food_type" : "Juices",
+    	},
+    	'my_object3' : {
+    		"restaurant_name" : "Anoosh",
+    		"food_type" : "Cookies",
+    	},
     }
     return render(request, 'detail.html', context)
